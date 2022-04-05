c59b77d0d7295f8e2f198d39c1e3e18e18e66aa7
Jose Miguel Duque Mejia - Ceiba Software <jose.duque@ceiba.com.co>
2022-04-04 22:10:31 -0500

    Images, Fonts and SASS  Optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
     entry: [ './src/index.js' ],
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js'
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js', '.html']
@@ -20,6 +22,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'postcss-loader',
+                  'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg)/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                },
             }
         ],
     },
@@ -28,6 +50,15 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve( __dirname, 'src', 'assets/images' ),
+                    to: 'assets/images'
+                }
+            ]
+        }),
     ]
 };
