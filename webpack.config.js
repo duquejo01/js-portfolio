bb376578f743a2a4ae594e109f0ae7dacd7602f7
Jose Miguel Duque Mejia - Ceiba Software <jose.duque@ceiba.com.co>
2022-04-05 21:25:56 -0500

    ENV Build Creation

--- webpack.config.js
+++ webpack.config.js
@@ -80,8 +80,6 @@ module.exports = {
         //         }
         //     ]
         // }),
-        new DotenvWebpackPlugin({
-            systemvars: true
-        })
+        new DotenvWebpackPlugin()
     ]
 };
